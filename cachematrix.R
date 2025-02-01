2c51bc351b940c94e98dd62af5169772b6e57e81
JBob92 <james.shutl@gmail.com>
2025-02-02 08:55:21 +1100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can store its inverse in an attempt to avoid repeated calculations.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL  # Start with no cached inverse
+  
+  ## Function to update the matrix (and reset the inverse)
+  set <- function(newMatrix) {
+    x <<- newMatrix
+    inverse <<- NULL  # Reset inverse when matrix changes
+  }
+  
+  ## Function to get the stored matrix
+  get <- function() x
+  
+  ## Function to store the inverse
+  setInverse <- function(newInverse) inverse <<- newInverse
+  
+  ## Function to get the stored inverse
+  getInverse <- function() inverse
+  
+  ## Return a list of these functions
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix. If the inverse has already been calculated, 
+##it retrieves it from the cache instead of recalculating.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()  # Get the cached inverse
+  
+  ## If the inverse is already stored, return it
+  if (!is.null(inverse)) {
+    message("Getting cached inverse...")
+    return(inverse)
+  }
+  
+  ## Otherwise, calculate the inverse
+  matrix <- x$get()  # Get the stored matrix
+  inverse <- solve(matrix, ...)  # Compute its inverse
+  x$setInverse(inverse)  # Save the inverse
+  
+  inverse  # Return the inverse
 }
+
+# As an exmaple of how it should work. Create a simple 2x2 matrix
+my_matrix <- matrix(c(4, 7, 2, 6), 2, 2)
+
+# Create a special matrix object
+cachedMatrix <- makeCacheMatrix(my_matrix)
+
+# Compute and cache the inverse (first time)
+cacheSolve(cachedMatrix)
+
+# Retrieve the cached inverse (second time)
+cacheSolve(cachedMatrix)
